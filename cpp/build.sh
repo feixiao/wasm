c3a7b76e682dcb02a743d6432b1385accec2cc7b
feixiao <feixiao2020@sina.com>
2025-03-11 13:21:47 +0800

    update

--- cpp/build.sh
+++ cpp/build.sh
@@ -1,7 +1,18 @@
+
+source "/Users/frank/wk/forbuild/emsdk-4.0.4/emsdk_env.sh"
+CMAKE_TOOLCHAIN_FILE=/home/user/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+
+
 rm build/ -rf
 mkdir build
-cd build
-cmake ../cpp
+cd build 
+emcmake cmake ../cpp
 make
-mv hello.js ../web/gen/
-mv hello.wasm ../web/gen/
+mv ../cpp/public/hello.js ../web/gen/hello.js
+
+
+# make
+emcmake cmake ../cpp -DJS_ONLY=ON  
+make 
+mv ../cpp/public/hello.wasm.js ../web/gen/hello.wasm.js
+mv ../cpp/public/hello.wasm.wasm ../web/gen/hello.wasm.wasm
